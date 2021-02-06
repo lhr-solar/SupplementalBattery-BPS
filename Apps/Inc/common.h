38a569a6c266d913b3a8b05700285ff5ac38e882
Manthan Upadhyaya <manthanand@gmail.com>
2021-02-06 11:46:54 -0600

    Added common.h file

---
+++ Apps/Inc/common.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2020 UT Longhorn Racing Solar */
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <ctype.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#endif
